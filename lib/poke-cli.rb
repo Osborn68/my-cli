0bcdb28448da037b3c63e462e3e8f668d2d9b596
Christopher Osborn <z68ozz@gmail.com>
2020-06-04 00:55:20 +0000

    add bundler

--- lib/poke-cli.rb
+++ lib/poke-cli.rb
@@ -1,5 +1,8 @@
 #application environment 
 
+require 'bunder/setup'
+Bundler.require(:default)
+
 require_relative './Poke-cli/api.rb'
 require_relative './Poke-cli/cli.rb'
 require_relative './Poke-cli/pokemon.rb'
